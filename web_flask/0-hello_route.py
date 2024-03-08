5185b69f3dadd05cc8094b9cc0a3b1144510acef
Ace <AceQuantumBit@gmail.com>
2024-03-08 12:21:17 -0600

    Pushing some documentation

--- web_flask/0-hello_route.py
+++ web_flask/0-hello_route.py
@@ -9,8 +9,13 @@ from flask import Flask
 """
 More documentation because idk where they want it
 """
+
 app = Flask(__name__)
 app.run(host='0.0.0.0', port=5000)
+
+"""
+Documentation here?
+"""
 
 @app.route('/')
 def home():
