e49379e85892418096f998cba25b0cfe6f0d4baa
Ace <AceQuantumBit@gmail.com>
2024-03-08 12:07:14 -0600

    Pushing some documentation

--- web_flask/0-hello_route.py
+++ web_flask/0-hello_route.py
@@ -6,9 +6,12 @@ Triple quotes for documentation
 
 from flask import Flask
 
+"""
+More documentation because idk where they want it
+"""
 app = Flask(__name__)
 app.run(host='0.0.0.0', port=5000)
 
 @app.route('/')
 def home():
-    return 'Hello HBNB'
+    return 'Hello HBNB!'
