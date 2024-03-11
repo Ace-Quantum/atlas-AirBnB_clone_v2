ae121d94cbaf2d4996b1e63f5e3fff9c690e6de0
Ace <AceQuantumBit@gmail.com>
2024-03-10 19:35:42 -0500

    Thank you Clay

--- web_flask/4-number_route.py
+++ web_flask/4-number_route.py
@@ -34,7 +34,7 @@ def task3(text="is cool"):
     return f"Python {text}"
 
 
-@app.route("/number/<n>", strict_slashes=False)
+@app.route("/number/<int:n>", strict_slashes=False)
 # @app.route("/number/", strict_slashes=False)
 def task4(n):
     if type(n) == int:
