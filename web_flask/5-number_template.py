a359f0bd45be7a5867f9c81967c98a1e61497c78
Ace <AceQuantumBit@gmail.com>
2024-03-10 20:20:41 -0500

    Ummmm?

---
+++ web_flask/5-number_template.py
@@ -0,0 +1,47 @@
+#!/usr/bin/python3
+
+"""Triple quotes for documentation"""
+
+from flask import Flask
+
+"""More documentation because idk where they want it"""
+
+app = Flask(__name__)
+
+"""Documentation here?"""
+
+
+@app.route("/", strict_slashes=False)
+def task0():
+    return "Hello HBNB!"
+
+
+@app.route("/hbnb", strict_slashes=False)
+def task1():
+    return "HBNB"
+
+
+@app.route("/c/<text>", strict_slashes=False)
+def task2(text):
+    text = text.replace("_", " ")
+    return f"C {text}"
+
+
+@app.route("/python/<text>", strict_slashes=False)
+@app.route("/python/", strict_slashes=False)
+def task3(text="is cool"):
+    text = text.replace("_", " ")
+    return f"Python {text}"
+
+
+@app.route("/number/<int:n>", strict_slashes=False)
+def task4(n):
+    return f"{n} is a number"
+
+
+@app.route("/number_template/<int:n>", strict_slashes=False)
+def task5(n):
+    return render_template('index.html')
+
+if __name__ == "__main__":
+    app.run(host="0.0.0.0", port=5000, debug=False)
