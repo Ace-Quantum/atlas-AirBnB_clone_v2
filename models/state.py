ff65e0f6365f64e1aed899d1d5097aa65b97a458
Ace <AceQuantumBit@gmail.com>
2024-03-12 16:25:53 -0500

    Honestly all I did was use review as a framework but let's see if it works before I feel smart for no reason

--- models/state.py
+++ models/state.py
@@ -17,4 +17,15 @@ class State(BaseModel, Base):
         cities = relationship("City", backref="state")
     else:
         name = ""
+
+        @property
+        def cities(self):
+            from models.city import City
+            city_list = []
+            all_cities = models.storage.all(City)
+            for city in all_cities.values():
+                if city.state_id == self.id:
+                    city_list.append(city)
+            return city_list
+
     cites = []
